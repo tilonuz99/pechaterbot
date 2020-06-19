eadec3e366e1e194ff12c2de5f3911a8b802a69f
tilonuz <59096186+tilonuz99@users.noreply.github.com>
2020-06-19 22:46:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ def callback_query(call):
 			yoz.write(rangi)
 	elif "stile" in call.data:
 		stile = call.data.replace("stile","")
-		style(call,pos,str(stile)+".ttf",matn,rangi,int(hajmi))
+		style(call,pos," "+str(stile)+".ttf",matn,rangi,int(hajmi))
 		image = open(str(chat_id)+'water.jpg', 'rb')
 		bot.edit_message_media(media=types.InputMedia(type='photo', media=image),chat_id=call.message.chat.id,message_id=call.message.message_id,reply_markup=stil())
 		
