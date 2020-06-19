591cd88e81b134b583b6e6ad30aa372bfb8c4da6
tilonuz <59096186+tilonuz99@users.noreply.github.com>
2020-06-19 19:52:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,9 @@ def colours():
 def stil():
     markup = InlineKeyboardMarkup()
     markup.row_width = 5
-    for i in range(5):
-        markup.add(InlineKeyboardButton(str(i),callback_data="stile{0}".format(i)))
+    for i in range(1,26):
+        for a in range(5):
+            markup.add(InlineKeyboardButton(str(a),callback_data="stile{a}".format(i)))
     return markup
 def rasm():
 	markup = InlineKeyboardMarkup()
